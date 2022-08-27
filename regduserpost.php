139427da91367f73652e72211934a92797a71184
Siddhartha Sen <55378140+SiddharthSen92@users.noreply.github.com>
2022-08-27 21:21:12 +0530

    Add files via upload

---
+++ regduserpost.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST['myregdname'];
+$email = $_POST['myregdemail'];
+$phone = $_POST['myregdphone'];
+$gender = $_POST['gender'];
+$password= $_POST['mypassword'];
+
+echo $name.", ".$email.", ".$phone.", ".$gender.", ".$password.".";
+
+?>
